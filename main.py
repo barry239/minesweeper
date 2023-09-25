458d5c8384af5d2cb408b9a57a00002a33a11a53
barry239 <thelreverseflash239@gmail.com>
2023-09-24 20:39:54 -0600

    Uncover cell

--- main.py
+++ main.py
@@ -19,8 +19,16 @@ class Minesweeper:
         while len(self.mines) < self.num:
             self.mines.add((random.randint(1, self.size), random.randint(1, self.size)))
 
-    def containsMine(self, x: int, y: int) -> bool:
-        return (x, y) in self.mines
+    def containsMine(self, row: int, col: int) -> bool:
+        return (row, col) in self.mines
+    
+    def uncoverCell(self, row: int, col: int) -> None:
+        count = 0
+        for i in range(max(1, row - 1), min(self.size + 1, row + 2)):
+            for j in range(max(1, col - 1), min(self.size + 1, col + 2)):
+                if (i, j) in self.mines: count += 1
+        
+        self.board[row - 1][col - 1] = str(count)
 
     def showMines(self) -> None:
         for mine in self.mines:
@@ -28,26 +36,44 @@ class Minesweeper:
 
     def displayBoard(self) -> None:
         for i, row in enumerate(self.board):
-            print(f' {i + 1:>2} | ', end='')
+            print(f"{' ' if self.size > 9 else ''}{i + 1:>2} | ", end='')
             print(' '.join(row))
-        print(f"{' ' * 4}+{'-' * self.size * 2}")
-        print(f"{' ' * 6}{' '.join([chr(i) for i in range(ord('a'), ord('a') + self.size)])}\n")
+        print(f"{' ' * (4 if self.size > 9 else 3)}+{'-' * self.size * 2}")
+        print(f"{' ' * (6 if self.size > 9 else 5)}{' '.join([chr(i) for i in range(ord('a'), ord('a') + self.size)])}\n")
 
+    def finishGame(self, msg: str) -> None:
+        self.showMines()
+        self.displayBoard()
+        print('[+]', msg)
+
+
+## Show main menu
 clear()
 print('Elija la dificultad:')
 print('1) Principiante - 9 x 9 casillas, 10 minas')
 print('2) Avanzado - 16 x 16 casillas, 40 minas\n')
 difficulty = int(input())
 
+## Create minesweeper
 mspr = Minesweeper(difficulty)
 mspr.generateMines()
 
+## Start game
 while True:
+    ## Display board
     clear()
-    print('[DEBUG]', mspr.mines)
     mspr.displayBoard()
+    print('[DEBUG]', mspr.mines)
 
-    x = int(input('Ingrese x: '))
-    y = int(input('Ingrese y: '))
+    ## Enter coordinates
+    row = int(input('Ingrese la fila: '))
+    col = int(input('Ingrese la columna: '))
 
-    if mspr.containsMine(x, y): mspr.showMines()
+    ## Check if a mine has been hit
+    if mspr.containsMine(row, col):
+        clear()
+        mspr.finishGame('Juego terminado, seleccionaste una mina')
+        break
+
+    ## Uncover cell
+    mspr.uncoverCell(row, col)
